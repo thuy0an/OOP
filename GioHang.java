9604bb01e2b624cc22888a8ebd2bff9fcb43af13
Thuy An <128052804+thuy0an@users.noreply.github.com>
2023-12-01 23:14:35 +0700

    Fix lại code của GioHang

---
+++ GioHang.java
@@ -0,0 +1,107 @@
+
+package BookStore;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import BookStore.KhachHang;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class GioHang {
+    private ArrayList<CT_GioHang> dsSanPham= new ArrayList<>();
+
+    public GioHang() {
+        
+    }
+
+    public ArrayList<CT_GioHang> getGioHang() {
+        return dsSanPham;
+    }
+
+    public void setGioHang(CT_GioHang GioHang) {
+        this.dsSanPham.add(GioHang);
+    }
+    
+    
+    
+    public void ghiGioHangVaoFile(String maKhachHang) {
+        String fileGioHang="GioHang";
+        fileGioHang=fileGioHang.concat(maKhachHang).concat(".txt");
+        System.out.println("Ten file ben ghiGioHang: " + fileGioHang);
+        try (PrintWriter printWriter = new PrintWriter(fileGioHang)) {
+            StringBuilder sb = new StringBuilder();
+            for (CT_GioHang giohang : this.getGioHang()) {
+
+                sb.append(giohang.getMaKhachhang()).append("#");
+                sb.append(giohang.getMaSach()).append("#");
+                sb.append(giohang.getTenSach()).append("#");
+                sb.append(giohang.getLoaiSach()).append("#");
+                sb.append(giohang.getSoLuong()).append("#");
+                sb.append(giohang.getThanhTien());
+                sb.append(System.lineSeparator()); //Xuống dòng
+            }
+            printWriter.println(sb.toString());
+        } catch (IOException e) {
+            System.out.println("Khong hop le" + e.getMessage());
+        }
+    }
+    
+    
+    
+    public  void xemGioHang(String makhachhang) {
+        String fileGioHang="GioHang";
+        fileGioHang=fileGioHang.concat(makhachhang).concat(".txt");
+        File file= new File(fileGioHang);
+        try (Scanner scanner = new Scanner(file))
+        {
+            System.out.println("\t+---------------------------------------------------------------------------------------+");
+            System.out.println("\t|                                 GIO HANG CUA BAN                                      |");
+            System.out.println("\t|_______________________________________________________________________________________|");
+            System.out.printf("\t| %-40s| %-8s| %-10s| %-10s| %-10s| \n", "Ten Sach", "Ma Sach", "Loai Sach", "So Luong", "Thanh Tien");
+            System.out.println("\t+-----------------------------------------+---------+-----------+-----------+-----------+\n");
+
+            while(scanner.hasNextLine()){ 
+                String line = scanner.nextLine();
+                String[] gioHang = line.split("#");
+                    String maKhachHang = gioHang[0];
+                    String maSach = gioHang[1];
+                    String tenSach = gioHang[2];
+                    String loaiSach = gioHang[3];
+                    String soLuong = gioHang[4];
+                    String thanhTien = gioHang[5];
+                    if (timGioHangTheoMaKhachHang(maKhachHang)) {
+                        System.out.printf("| %-40s| %-8s| %-10s| %-10s| %-10s|\n",tenSach, maSach, loaiSach, soLuong, thanhTien);
+                    }
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println("Không tìm thấy file");
+        }
+    }
+    private boolean timGioHangTheoMaKhachHang(String maKhachHang) {
+        File user= new File("user.txt");
+        File userLogin= new File("userLogin.txt");
+        try {
+            Scanner scan= new Scanner(user);
+            Scanner scan2= new Scanner(userLogin);
+            String taikhoanDangNhap=scan2.nextLine();
+            String tkDangNhap[]=taikhoanDangNhap.split("#");
+            while ( scan.hasNextLine())
+            {
+                String dulieuTaiKhoan=scan.nextLine();
+                String taiKhoan[]=dulieuTaiKhoan.split("#");
+                if ( taiKhoan[0].equalsIgnoreCase(tkDangNhap[0]) && taiKhoan[1].equalsIgnoreCase(tkDangNhap[1]) && taiKhoan[2].equalsIgnoreCase(maKhachHang))
+                    return true;
+            }
+            
+        } catch (FileNotFoundException ex) {
+            System.out.println("Không tìm thấy file !!!");
+        }
+        return false;
+    }
+}    
+ 
